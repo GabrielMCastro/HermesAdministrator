dddf2ba7cbf2df87c23ac86e4c1db92054e55055
Mateo Castro <mateocastro@Mateos-MacBook-Pro.local>
2018-10-21 15:17:40 -0500

    A little bit of work

--- hermes_administrator/src/components/FlightsPane.js
+++ hermes_administrator/src/components/FlightsPane.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { fetchFlights, newFlightSelected} from '../DataManager';
+import { fetchScanners, fetchFlights, newFlightSelected} from '../DataManager';
 import '../styles/panes.css';
 
 class FlightsPane extends Component {
@@ -9,13 +9,19 @@ class FlightsPane extends Component {
             <div className='paneBody'>
                 <div className='header'>
                     <div className='paneHeader'>Flights</div>
-                    <button id='update' onClick={this.props.fetchFlights}>Refresh</button>
+                    <button id='update' onClick={this.fetchUpdate}>Refresh</button>
                 </div>
                 <div className='listContainer'>
                     {this.props.flights && this.props.flights.map(this.mapfunction)}
                 </div>
             </div>
         );
+    }
+
+    fetchUpdate = () =>
+    {
+        this.props.fetchFlights()
+        this.props.fetchScanners()
     }
 
     mapfunction = flight => {
@@ -35,6 +41,7 @@ const mapStateToProps = (state) => {
 const mapDispatchToProps = (dispatch) => {
     return {
         fetchFlights: () => dispatch(fetchFlights()),
+        fetchScanners: () => dispatch(fetchScanners()),
         newFlightSelected: (flightid) => dispatch(newFlightSelected(flightid)),
     };
 };
